4744955b6888053fa63f0a73cfcb20ee8319b318
hoeding <hoeding@gmail.com>
2025-12-27 12:51:18 -0600

    ZulFarrack

--- Vanilla/Alliance_Zul_Farrack.lua
+++ Vanilla/Alliance_Zul_Farrack.lua
@@ -4,11 +4,55 @@ guidelime.registerguide([[
 [NX The Temple of Atal'Hakkar - Alliance]
 [D description]
 
+Hinterlands
+Accept[QA2988]--Witherbark Cages, 40 55, Hinterlands
+Complete[QC2988]--Witherbark Cages, Hinterlands
+Turn in [QT2988]--Witherbark Cages, Hinterlands
+Accept[QA2989]--The Altar of Zul, 40 58, Hinterlands
+Complete[QC2989]--The Altar of Zul, Hinterlands
+Turn in [QT2989]--The Altar of Zul, Hinterlands
+Accept[QA2990]--Thadius Grimshade, 40 57, Hinterlands
 
+Ironforge
+Accept[QA2769]--The Brassbolt Brothers, 40 56, Ironforge
+Accept[QA2861]--Tabetha's Task, 40 56, IF/SW
+
+Blasted Lands
+Turn in [QT2990]--Thadius Grimshade, Blasted Lands
+Accept[QA2991]--Nekrum's Medallion, 40 57, Blasted Lands
+
+Booty Bay
+Accept[QA2864]--Tran'rek, 40 55, Booty Bay
+
+Wetlands
+Turn in [QT2861]--Tabetha's Task, Dustwallow Marsh
+Accept[QA2846]--Tiara of the Deep, 40 56, Dustwallow Marsh
+
+Gadgetzan
+Turn in [QT2769]--The Brassbolt Brothers, Shimmering Flats
+Accept[QA2770]--Gahz'rilla, 40 61, Shimmering Flats
+Accept[QA2768]--Divino-matic Rod 40 57, Gadgetzan
+Turn in [QT2864]--Tran'rek, Gadgetzan
+Accept[QA2865]--Scarab Shells, 40 55, Gadgetzan
+Accept[QA3042]--Troll Temper, 40 55, Gadgetzan
+
+Zul'Farrak
+Complete[QC2865]--Scarab Shells, ZF
+Complete[QC2768]--Divino-matic Rod ZF stairs
+Complete[QC2991]--Nekrum's Medallion, 40 57, ZF
+Complete[QC2846]--Tiara of the Deep, ZF
+Complete[QC2770]--Gahz'rilla, ZF
+Complete[QC3042]--Troll Temper, ZF
+
+Gadgetzan
+Turn in [QT2768]--Divino-matic Rod, Gadgetzan
+Turn in [QT2865]--Scarab Shells, Gadgetzan
+Turn in [QT2770]--Gahz'rilla, Shimmering Flats
+Turn in [QT3042]--Troll Temper, Gadgetzan
+
+Dustwallow Marsh
+Turn in [QT2846]--Tiara of the Deep, Dustwallow Marsh
+
+Blasted Lands
+Turn in [QT2991]--Nekrum's Medallion, 40 57, Blasted Lands
 ]], "Hoedings Guide")
-
-
-Accept[QA
-Complete[QC
-Turn in [QT
-
