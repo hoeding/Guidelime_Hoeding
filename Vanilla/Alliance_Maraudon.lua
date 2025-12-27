742277e7ad33da6d1294b42d6a3651466908b4e6
hoeding <hoeding@gmail.com>
2025-12-27 14:28:29 -0600

    Maraudon

---
+++ Vanilla/Alliance_Maraudon.lua
@@ -0,0 +1,55 @@
+guidelime.registerguide([[
+[GA Alliance]
+[N Maraudon - Alliance]
+[NX The Temple of Atal'Hakkar - Alliance]
+[D description]
+
+Theramore
+Accept[QA7070]--Shadowshard Fragments, 39 52, Theramore
+
+Nijels Point
+Accept[QA7065]--Corruption of Earth and Seed, 45 62, Nijels Point
+Accept[QA7041]--Vyletongue Corruption, 41 57, Nijels Point
+
+Central Desolace
+Accept[QA7028]--Twisted Evils, 41 57, Central Desolace
+
+South Desolace
+Accept[QA7067]--The Pariah's Instruction, 39 58, South Desolace
+
+Outside Maraudon
+Accept[QA7044]--Legends of Maraudon, 41 59, Maraudon Orange
+Complete[QC7044]--Legends of Maraudon, Purple and Orange
+Complete[QC7070]--Shadowshard Fragments, Maraudon Purple entrance
+Kolk - Portal room outsie
+Gelk - Outside Purple
+Magra - Outside Orange
+
+Inside Maraudon
+Maraudos - Inside Purple
+Turn in[QT7044]--Legends of Maraudon, Maraudon Celebras
+Accept[QA7046]--The Scepter of Celebras, 41 59, Maraudon Celebras
+Turn in[QT7046]--The Scepter of Celebras, Maraudon Celebras
+Complete[QC7065]--Corruption of Earth and Seed, Maraudon - Princess
+Accept[QA7066]--Seed of Life, 39 62, Maraudon - Princess
+Complete[QC7067]--The Pariah's Instruction
+Complete[QC7028]--Twisted Evils, Maraudon inside and out
+Complete[QC7041]--Vyletongue Corruption, Maraudon - Orange
+
+Nijels Point
+Turn in[QT7065]--Corruption of Earth and Seed, Nijels Point
+Turn in[QT7041]--Vyletongue Corruption, Nijels Point
+
+South Desolace
+Turn in[QT7067]--The Pariah's Instruction, South Desolace
+
+Central Desolace
+Turn in[QT7028]--Twisted Evils, Central Desolace
+
+Theramore
+Turn in[QT7070]--Shadowshard Fragments, Theramore
+
+Moonglade
+Turn in[QT7066]--Seed of Life, Moonglade
+
+]], "Hoedings Guide")
