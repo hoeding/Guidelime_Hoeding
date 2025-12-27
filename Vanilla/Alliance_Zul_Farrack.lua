ff7c9f4935d89b45f840ccfe839ae35d5803f1b8
hoeding <hoeding@gmail.com>
2025-12-27 11:31:43 -0600

    initial

---
+++ Vanilla/Alliance_Zul_Farrack.lua
@@ -0,0 +1,14 @@
+guidelime.registerguide([[
+[GA Alliance]
+[N Zul'Farrak - Alliance]
+[NX The Temple of Atal'Hakkar - Alliance]
+[D description]
+
+
+]], "Hoedings Guide")
+
+
+Accept[QA
+Complete[QC
+Turn in [QT
+
