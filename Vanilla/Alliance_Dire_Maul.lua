4be3e8e2df9e0ee0998dd85869702bb1634129bd
hoeding <hoeding@gmail.com>
2025-12-31 02:55:24 -0600

    DM ST ZG incomplete

---
+++ Vanilla/Alliance_Dire_Maul.lua
@@ -0,0 +1,43 @@
+DME
+
+Accept[QA7494]--Feathermoon Stronghold, 54 68, SW/IF/Darnassus
+Turn in[QT7494]--Feathermoon Stronghold, Feralas
+Accept[QA7488]--Lethtendris's Web, 54 68, Feralas
+Complete[QC7488]--Lethtendris's Web, DME
+Turn in[QT7488]--Lethtendris's Web, Feralas
+
+Accept[QA5527]--A Reliquary of Purity, 56 72, Moonglade
+Complete[QC5527]--A Reliquary of Purity, Silithus
+Turn in[QT5527]--A Reliquary of Purity, Moonglade
+Accept[QA5526]--Shards of the Felvine, 56 72, Moonglade
+Complete[QC5526]--Shards of the Felvine, DME
+Turn in[QT5526]--Shards of the Felvine, Moonglade
+
+Accept[QA7441]--Pusillin and the Elder Azj'Tordin, 54 69, Feralas East
+Complete[QC7441]--Pusillin and the Elder Azj'Tordin, DME
+Turn in[QT7441]--Pusillin and the Elder Azj'Tordin, Feralas East
+
+
+
+DMW
+
+Accept[QA7461]--The Madness Within, 56 72, DMW
+Complete[QC7461]--The Madness Within, DMW
+Turn in[QT7461]--The Madness Within, DMW
+
+Accept[QA7877]--The Treasure of the Shen'dralar, 56 72, DMW
+Turn in[QT7877]--The Treasure of the Shen'dralar, DMW
+
+Accept[QA
+Complete[QC
+Turn in[QT
+
+Accept[QA
+Complete[QC
+Turn in[QT
+
+
+
+Accept[QA
+Complete[QC
+Turn in[QT
---
+++ Vanilla/ZG_intro_chain.lua
@@ -0,0 +1,30 @@
+
+Accept[QA3520]--Screecher Spirits, 40 54, East Tanaris
+Complete[QC3520]--Screecher Spirits, Feralas
+Turn in[QT3520]--Screecher Spirits, East Tanaris
+
+Accept[QA3527]--The Prophecy of Mosh'aru, 40 57, East Tanaris
+Complete[QC3527]--The Prophecy of Mosh'aru, Zul'Farrak
+Turn in[QT3527]--The Prophecy of Mosh'aru, East Tanaris
+
+Accept[QA4787]--The Ancient Egg, 40 61, East Tanaris
+Complete[QC4787]--The Ancient Egg, Hinterlands Jintha'Alor
+Turn in[QT4787]--The Ancient Egg, East Tanaris
+
+Accept[QA3528]--The God Hakkar, 40 64, East Tanaris
+Complete[QC3528]--The God Hakkar, Sunken Temple
+Turn in[QT3528]--The God Hakkar, East Tanaris
+
+Accept[QA5065]--The Lost Tablets of Mosh'aru, 40 69, East Tanaris
+Complete[QC5065]--The Lost Tablets of Mosh'aru, Eastern Plaguelands
+Turn in[QT5065]--The Lost Tablets of Mosh'aru, East Tanaris
+
+Accept[QA4788]--The Final Tablets, 40 69, East Tanaris
+Complete[QC4788]--The Final Tablets, 40 69, LBRS
+Turn in[QT4788]--The Final Tablets, 40 69, East Tanaris
+
+Accept[QA8181]--Confront Yeh'kinya, 40 69, East Tanaris
+Turn in[QT8181]--Confront Yeh'kinya, East Tanaris
+
+Accept[QA8182]--The Hand of Rastakhan, 40 69, East Tanaris
+Turn in[QT8182]--The Hand of Rastakhan, STV
