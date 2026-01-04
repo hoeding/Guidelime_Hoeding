e19bfe425d5df8b9359dec350e5f96638f82647b
hoeding <hoeding@gmail.com>
2026-01-03 22:09:02 -0600

    Work towards BRD quests

---
+++ Vanilla/Alliance-Blackrock_Depths_one.lua
@@ -0,0 +1,13 @@
+Guidelime.registerGuide(
+    [[
+[N 14-20 Blackrock Depths Early Quests] --Name
+[D Descriptive description] --Description
+[NX Blackrock Depths Late Quests] --Next Guide
+[GA Alliance] --Faction, races, class global
+
+
+    ]], "Hoeding's Guide")
+
+Accept[QA
+Complete[QC
+Turn in[QT
---
+++ Vanilla/Alliance-Blackrock_Depths_two.lua
@@ -0,0 +1,9 @@
+Guidelime.registerGuide(
+    [[
+[N 14-20 Blackrock Depths Early Quests] --Name
+[D Descriptive description] --Description
+[NX Blackrock Depths Late Quests] --Next Guide
+[GA Alliance] --Faction, races, class global
+
+
+    ]], "Hoeding's Guide")
