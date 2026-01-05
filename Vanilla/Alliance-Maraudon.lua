41ac40987b4eb9fa9d61af0f14607a49a7eda5d8
hoeding <hoeding@gmail.com>
2026-01-05 11:40:21 -0600

    Maraudon fixups

--- Vanilla/Alliance-Maraudon.lua
+++ Vanilla/Alliance-Maraudon.lua
@@ -18,33 +18,46 @@ South Desolace
 Accept[QA7067]--The Pariah's Instruction, 39 58, South Desolace
 
 Outside Maraudon
+Kill the Nameless Prophet for [CI17757]
+Kolk - Portal room outside
 Accept[QA7044]--Legends of Maraudon, 41 59, Maraudon Orange
-Complete[QC7044]--Legends of Maraudon, Purple and Orange
-Complete[QC7070]--Shadowshard Fragments, Maraudon Purple entrance
-Kolk - Portal room outsie
-Gelk - Outside Purple
-Magra - Outside Orange
 
-Inside Maraudon
+Magra - Outside Orange
+Complete[QC7041][O]--Vyletongue Corruption, Maraudon - Orange
+Enter Mara Orange
+Fifth Khan Veng - Inside Orange
+Noxxion [CI17702]--Celebrian rod
+
+
+
+Leave orange and head to purple
+Second Khan Gelk - Outside Purple
+
+Complete[QC7044][O]--Legends of Maraudon, Purple and Orange
+Complete[QC7070]--Shadowshard Fragments, Maraudon Purple entrance
+Lord Vyletongue[CI17703]--Celebrian Diamond
+
+
 Maraudos - Inside Purple
+Complete[QC7044][O]--Legends of Maraudon, Purple and Orange
+Complete[QC7028][O]--Twisted Evils, Maraudon inside and out
 Turn in[QT7044]--Legends of Maraudon, Maraudon Celebras
+book on ground
 Accept[QA7046]--The Scepter of Celebras, 41 59, Maraudon Celebras
 Turn in[QT7046]--The Scepter of Celebras, Maraudon Celebras
 Complete[QC7065]--Corruption of Earth and Seed, Maraudon - Princess
 Accept[QA7066]--Seed of Life, 39 62, Maraudon - Princess
 Complete[QC7067]--The Pariah's Instruction
-Complete[QC7028]--Twisted Evils, Maraudon inside and out
-Complete[QC7041]--Vyletongue Corruption, Maraudon - Orange
-
-Nijels Point
-Turn in[QT7065]--Corruption of Earth and Seed, Nijels Point
-Turn in[QT7041]--Vyletongue Corruption, Nijels Point
 
 South Desolace
 Turn in[QT7067]--The Pariah's Instruction, South Desolace
 
 Central Desolace
 Turn in[QT7028]--Twisted Evils, Central Desolace
+
+Nijels Point
+Turn in[QT7041]--Vyletongue Corruption, Nijels Point
+Turn in[QT7065]--Corruption of Earth and Seed, Nijels Point
 
 Theramore
 Turn in[QT7070]--Shadowshard Fragments, Theramore
