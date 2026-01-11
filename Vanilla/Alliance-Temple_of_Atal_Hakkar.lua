39b532f64f271543a222019e5891e9b0c3b270a1
hoeding <hoeding@gmail.com>
2026-01-11 08:34:40 -0600

    Haze of Evil

--- Vanilla/Alliance-Temple_of_Atal_Hakkar.lua
+++ Vanilla/Alliance-Temple_of_Atal_Hakkar.lua
@@ -3,8 +3,18 @@ Guidelime.registerGuide([[
 [N 46-53 Temple of Atal'Hakkar Quests]
 --[NX The Temple of Atal'Hakkar - Alliance]
 [D description]
+[F Marshal's Refuge]
+Accept[QA4141]--Muigin and Larion, 47 63, Un'Goro
+Complete[QC4141]--Muigin and Larion, 47 63, Un'Goro
+Turn in[QT4141]--Muigin and Larion, 47 63, Un'Goro
+Accept[QA4142]--A Visit to Gregan, 47 63, Un'Goro
+
 [F Tanaris]
 Accept[QA4787]--The Ancient Egg, 40 61, East Tanaris
+
+[F Feathermoon Stronghold]
+Turn in[QT4142]--A Visit to Gregan, Feralas
+Accept[QA4143]--Haze of Evil, 47 63, Feralas
 
 [F Stormwind]
 Accept[QA1448]--In Search of the Temple, 38 53, Stormwind
@@ -55,13 +65,21 @@ Accept[QA1475]--Into the Temple of Atal'Hakkar, 38 61, Stormwind
 
 [F Nethergarde Keep]
 Sunken Temple
-Complete[QC1475]--Into the Temple of Atal'Hakkar, Sunken Temple
+Stairs on far right Turn in and kill trash in boss spawn area[QT3446]--Into the Depths, ST
+Complete along the way[QC1475][O]--Into the Temple of Atal'Hakkar, Sunken Temple
+Complete along the way[QC4143][O]--Haze of Evil, Sunken Temple
+Stairs second from the right, clear the circle and do the statues.
+S, N, SW, SE, NW, NE
+Drop down, kill Atal'arion
+Turn in at statue[QT3447]--Secret of the Circle, ST
+Stairs second from left, clear the circle and minibosses.
 Complete[QC1446]--Jammal'an the Prophet, Sunken Temple
+Kill all dragons.
 Accept[QA3373]--The Essence of Eranikus, 48 66, Sunken Temple drop
 Complete[QC3373]--The Essence of Eranikus, Sunken Temple
 Turn in[QT3373]--The Essence of Eranikus, Sunken Temple
-Turn in[QT3447]--Secret of the Circle, ST
-Turn in[QT3446]--Into the Depths, ST
+
+[QC1475]--Into the Temple of Atal'Hakkar, Sunken Temple
 Complete[QC3528]--The God Hakkar, Sunken Temple
 
 [F Stormwind]
@@ -73,4 +91,8 @@ Turn in[QT1446]--Jammal'an the Prophet, Hinterlands
 [F Tanaris]
 Turn in[QT3528]--The God Hakkar, East Tanaris
 EPL/LBRS[QA5065][O] quest pickup--The Lost Tablets of Mosh'aru, 40 69, East Tanaris
+
+[F Marshal's Refuge]
+Turn in[QT4143]--Haze of Evil, Un'Goro
+
 ]], "Hoeding's Guide")
